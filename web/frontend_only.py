dc41c81f3afa76dfa89db4aae6b9269f13fb8740
Arne Binder <arne.b.binder@gmail.com>
2019-09-03 20:43:04 +0200

    send logic works

--- web/frontend_only.py
+++ web/frontend_only.py
@@ -19,97 +19,80 @@ async_mode = 'threading'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+NAMESPACE = '/convqa'
+QUEUE = 'convqa_out'
+QUEUE_EXT = 'convqa_in'
 #socketio = SocketIO(app, async_mode=async_mode)
-socketio = SocketIO(app, async_mode=async_mode, client_manager=KafkaChannelsManager(channel='convqa_out'))
+socketio = SocketIO(app, async_mode=async_mode, client_manager=KafkaChannelsManager(channel=QUEUE))
 #socketio = SocketIO(app, async_mode='threading', client_manager=KafkaManager(channel='convqa-out'))
 
 
 @app.route('/')
 def index():
-    return render_template('chat_flink.html', async_mode=socketio.async_mode)
+    return render_template('chat_flink.html', async_mode=socketio.async_mode, namespace=NAMESPACE)
 
 
-@socketio.on('ask', namespace='/convqa')
+@socketio.on('ask', namespace=NAMESPACE)
 def ask(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
     message_data = message['data']
-    user_room = request.sid
-    print(f'message_data: {message_data} (room: {user_room})')
-    emit('my_response', {'data': message['data'], 'count': session['receive_count']})
-    emit('answer', {'user_input': message_data, 'channel': 'convqa_in'})
+    #user_room = request.sid
+    #print(f'message_data: {message_data} (room: {user_room})')
+    emit('question', {'question': message['data']})
+    emit('answer', {'user_input': message_data, 'channel': QUEUE_EXT})
 
 
-@socketio.on('my_event', namespace='/convqa')
+@socketio.on('my_event', namespace=NAMESPACE)
 def echo_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response', {'data': message['data']})
+    emit('system_message', {'data': message['data']})
 
 
-@socketio.on('my_broadcast_event', namespace='/convqa')
+@socketio.on('my_broadcast_event', namespace=NAMESPACE)
 def broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
+    emit('system_message', {'data': message['data']}, broadcast=True)
 
 
-@socketio.on('join', namespace='/convqa')
+@socketio.on('join', namespace=NAMESPACE)
 def join(message):
     join_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
+    emit('system_message', {'data': 'In rooms: ' + ', '.join(rooms())})
 
 
-@socketio.on('leave', namespace='/convqa')
+@socketio.on('leave', namespace=NAMESPACE)
 def leave(message):
     leave_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
+    emit('system_message', {'data': 'In rooms: ' + ', '.join(rooms())})
 
 
-@socketio.on('close_room', namespace='/convqa')
+@socketio.on('close_room', namespace=NAMESPACE)
 def close(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
-                         'count': session['receive_count']},
-         room=message['room'])
+    emit('system_message', {'data': 'Room ' + message['room'] + ' is closing.'}, room=message['room'])
     close_room(message['room'])
 
 
-@socketio.on('my_room_event', namespace='/convqa')
+@socketio.on('my_room_event', namespace=NAMESPACE)
 def send_room_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         room=message['room'])
+    emit('system_message', {'data': message['data']}, room=message['room'])
 
 
-@socketio.on('disconnect_request', namespace='/convqa')
+@socketio.on('disconnect_request', namespace=NAMESPACE)
 def disconnect_request():
     @copy_current_request_context
     def can_disconnect():
         disconnect()
 
-    session['receive_count'] = session.get('receive_count', 0) + 1
     # for this emit we use a callback function
     # when the callback function is invoked we know that the message has been
     # received and it is safe to disconnect
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']},
-         callback=can_disconnect)
+    emit('system_message', {'data': 'Disconnected!'}, callback=can_disconnect)
 
 
-@socketio.on('connect', namespace='/convqa')
+@socketio.on('connect', namespace=NAMESPACE)
 def connect():
     print('Client connected')
-    emit('my_response', {'data': 'Connected', 'count': 0})
+    emit('system_message', {'data': 'Connected'})
 
 
-@socketio.on('disconnect', namespace='/convqa')
+@socketio.on('disconnect', namespace=NAMESPACE)
 def disconnect():
     print('Client disconnected', request.sid)
 
