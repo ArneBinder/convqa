e850ca080ba3ae79577d2a453accaadd8e90f308
Arne Binder <arne.binder@dfki.de>
2019-10-28 18:02:45 +0100

    add missing frontend files

---
+++ web/frontend/precache-manifest.d79d5f716e82de57371b4d2c24bdf0b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "adcacfd03df09fe0c633",
+    "url": "js/chunk-vendors.a883e1d7.js"
+  },
+  {
+    "revision": "7e570a40ae78f87f8ab3",
+    "url": "js/app.bf2f96fa.js"
+  },
+  {
+    "revision": "f4b53e8a3d536f01d4da250a5a783a2e",
+    "url": "index.html"
+  },
+  {
+    "revision": "7e570a40ae78f87f8ab3",
+    "url": "css/app.12baf98c.css"
+  }
+];
